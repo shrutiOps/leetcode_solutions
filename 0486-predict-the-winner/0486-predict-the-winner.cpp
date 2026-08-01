1f23c035c744905581e56d63118a9ed0ada0eabb
shrutiOps <shrutiomer2005@gmail.com>
2026-08-01 22:20:44 +0530

    Time: 94 ms (20.33%), Space: 9.7 MB (56.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,11 +3,11 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         return helper(nums, 0, nums.size() - 1) >= 0;
     }
+    
     int helper(vector<int>& nums, int i, int j) {
         if (i == j) return nums[i]; 
         int pickLeft  = nums[i] - helper(nums, i + 1, j);
         int pickRight = nums[j] - helper(nums, i, j - 1);
-
         return max(pickLeft, pickRight);
     }
 };
